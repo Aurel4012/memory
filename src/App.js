c07cbcdc71f28882d79cee88fbfe4db21ca1e2f7
Aurel4012 <33459190+Aurel4012@users.noreply.github.com>
2018-06-28 17:24:29 +0200

    gestion evenement
    ajout gestionevenement et premiere condition (toute les 2secondes)

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import './App.css'
 import GuessCount from './GuessCount'
 
 class App extends Component {
+	handleCardClick(card){ 
+		// gestion des events
+		console.log(card,'clicked');
+	}
   render() {
+  	const won = new Date().getSeconds() % 2 === 0
     return (
+    	
     	<div className="memory">
     	  <GuessCount guesses={0}/>	
-    	  <Card card="😋" feedback="hidden" />
-    	  <Card card="❤" feedback="justMatched" />
-    	  <Card card="🎩" feedback="JustMismached" />
-    	  <Card card="👼" feedback="visible" />
-    	  <Card card="👾" feedback="hidden" />
-    	  <Card card="🍺" feedback="justMatched" />
+    	  <Card card="😋" feedback="hidden" 		onClick={this.handleCardClick}/>
+    	  <Card card="❤" feedback="justMatched" 	onClick={this.handleCardClick}/>
+    	  <Card card="🎩" feedback="JustMismached" 	onClick={this.handleCardClick}/>
+    	  <Card card="👼" feedback="visible" 		onClick={this.handleCardClick}/>
+    	  <Card card="👾" feedback="hidden" 		onClick={this.handleCardClick}/>
+    	  <Card card="🍺" feedback="justMatched" 	onClick={this.handleCardClick}/>
+    	  {won && <p>Gagné</p>}
     	</div>
     )
   }
